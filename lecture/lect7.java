aff9f432d6af63253c677d1a9c501cc29e450aa4
Stephen <Stephen@CLOUD>
2022-11-19 22:18:36 +0800

    disc03

---
+++ lecture/lect7.java
@@ -0,0 +1,34 @@
+public class AList {
+    private int[] items;
+    private int size;
+
+    public AList() {
+        items = new int[100];
+        size = 0;
+    }
+
+    public void addLast(int x) {
+        items[size] = x;
+        size += 1;
+    }
+
+    public int removeLast(int x) {
+
+    }
+
+    public int getLast() {
+        return items[size - 1];
+    }
+
+    public int get(int i) {
+        return items[i];
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+}
+
+
